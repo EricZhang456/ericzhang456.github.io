f553839d65a45daf424a0fe4d0ac7a405738069a
Eric Zhang <ericxiang456@gmail.com>
2020-04-02 15:40:58 +0800

    k

---
+++ Erics_Site/Blog/tiao_mu/2020/4/2_The_Bizarre_Versions_of_QQ_files/2_The_Bizarre_Versions_of_QQ.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20200402
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('2_The_Bizarre_Versions_of_QQ_files/2_The_Bizarre_Versions_of_QQMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
--- Erics_Site/Main_files/Main.js
+++ Erics_Site/Main_files/Main.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20200401
+// Created by iWeb 3.0.4 local-build-20200402
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Main_files/MainMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
