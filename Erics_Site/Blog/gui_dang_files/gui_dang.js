ad1dcfbec9dccc4ca3ecd3ec5d9018c6e40fe982
Eric Zhang <ericxiang456@gmail.com>
2020-04-18 08:55:54 +0800

    wip

--- Erics_Site/Blog/gui_dang_files/gui_dang.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20200402
-
-setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('gui_dang_files/gui_dangMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- Erics_Site/Main_files/Main.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20200403
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Main_files/MainMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
