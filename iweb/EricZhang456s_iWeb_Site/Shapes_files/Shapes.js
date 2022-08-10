893f1494f560b3156962d4e7e60b1639fc1f9d44
Eric Zhang <ericxiang456@gmail.com>
2022-08-10 13:00:01 +0800

    upd

---
+++ iweb/EricZhang456s_iWeb_Site/Shapes_files/Shapes.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20220810
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(-3.0902,5.1430),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(-4.4589,4.0148),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Shapes_files/ShapesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
