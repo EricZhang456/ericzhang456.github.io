c4617afa530f42c2cdfcfa8301fe1dc076e995c9
Eric Zhang <ericxiang456@gmail.com>
2022-08-10 12:40:08 +0800

    upd

---
+++ iweb/EricZhang456s_iWeb_Site/Widgets_files/Widgets.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0 local-build-20220810
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Widgets_files/WidgetsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
