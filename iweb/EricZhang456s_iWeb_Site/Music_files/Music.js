ce089aa52c1a9ee534a0b8927d0f57461f7be448
Eric Zhang <ericxiang456@gmail.com>
2022-08-10 12:29:08 +0800

    uPdate

--- iweb/EricZhang456s_iWeb_Site/Music_files/Music.js
+++ iweb/EricZhang456s_iWeb_Site/Music_files/Music.js
@@ -11,6 +11,6 @@ setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Music_files/MusicMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
