e85f4065e850ffa87e1edab6c075aaf0e8e4596e
Eric Zhang <ericxiang456@gmail.com>
2022-08-10 12:23:43 +0800

    uPDATE

---
+++ iweb/EricZhang456s_iWeb_Site/Blog/Blog_files/Blog.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20220810
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("https://ericzhang456.github.io/iweb/EricZhang456s_iWeb_Site/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
